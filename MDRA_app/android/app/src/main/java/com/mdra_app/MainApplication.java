f74106c214dff3341fa4af8c6cfceda51699ce81
Jeffery <jefferyfarlyprovencher@hotmail.com>
2018-06-15 14:35:59 -0400

    Ionicons added

--- MDRA_app/android/app/src/main/java/com/mdra_app/MainApplication.java
+++ MDRA_app/android/app/src/main/java/com/mdra_app/MainApplication.java
@@ -8,8 +8,11 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.oblador.vectoricons.VectorIconsPackage;
+
 import java.util.Arrays;
 import java.util.List;
+
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -23,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage()
+          , new VectorIconsPackage()
       );
     }
 
